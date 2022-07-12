4d7ad909a249391bfbc43503e41fa6bfa046331a
clearbluejar <3752074+clearbluejar@users.noreply.github.com>
2022-07-12 17:06:32 -0400

    added devcontainer awesomeness

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,18 @@
+# Create local venv
+python3 -m venv .env
+source .env/bin/activate
+
+# Download pyi typings for Ghidra Version
+PYI_WHL_DOWNLOAD_URL=$(curl -s https://api.github.com/repos/clearbluejar/ghidra-pyi-generator/releases | jq -r ".[] | .assets[] | .browser_download_url" | grep "${GHIDRA_VERSION}" | grep whl)
+
+# Ensure have valid versions
+if [ -z "$PYI_WHL_DOWNLOAD_URL" ] || [ -z "$GHIDRA_VERSION" ]; then
+    echo "Error: Failed to get PYI_WHL_DOWNLOAD_URL:${PYI_WHL_DOWNLOAD_URL} filtering on GHIDRA_VERSION:${GHIDRA_VERSION}"
+    RELEASES=$(curl -s https://api.github.com/repos/clearbluejar/ghidra-pyi-generator/releases | jq -r ".[] | .assets[] | .browser_download_url")
+    echo "Possible releases here: ${RELEASES}"
+    exit 1 ## error out if they are empty
+fi
+
+echo $PYI_WHL_RELEASE_URLS
+echo $PYI_WHL_DOWNLOAD_URL
+pip install "${PYI_WHL_DOWNLOAD_URL}"
