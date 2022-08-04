dbf57d35da17a090e74a8195aba4adbcf9000d0e
clearbluejar <3752074+clearbluejar@users.noreply.github.com>
2022-08-04 00:17:30 -0400

    added ghidra-bridge method

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,7 +2,7 @@
 python3 -m venv .env
 source .env/bin/activate
 
-# Download pyi typings for Ghidra Version
+# Download latest pyi typings for Ghidra Version
 PYI_WHL_DOWNLOAD_URL=$(curl -s https://api.github.com/repos/clearbluejar/ghidra-pyi-generator/releases | jq -r ".[] | .assets[] | .browser_download_url" | grep "${GHIDRA_VERSION}" | grep whl)
 
 # Ensure have valid versions
@@ -16,3 +16,9 @@ fi
 echo $PYI_WHL_RELEASE_URLS
 echo $PYI_WHL_DOWNLOAD_URL
 pip install "${PYI_WHL_DOWNLOAD_URL}"
+
+# Download latest Ghidra Bridge
+pip install ghidra_bridge
+
+# Install bridge scripts
+python -m ghidra_bridge.install_server .ghidra_bridge
