cc8f9234b7810c6f84dea821215a2c9f1598ad22
clearbluejar <3752074+clearbluejar@users.noreply.github.com>
2025-06-27 14:56:27 +0000

    update for workshop

--- sample_pyhidra.py
+++ sample_pyhidra.py
@@ -1,4 +1,3 @@
-import os
 import pyghidra
 
 # Section to make autocomplete work
@@ -14,13 +13,14 @@ PROJECT_LOCATION = '.ghidra_projects'
 
 pyghidra.start(True)  # setting Verbose output
 
-from ghidra.program.model.listing import Program
+from ghidra.program.model.listing import Program  # noqa
 
 with pyghidra.open_program("/bin/ls", project_name=PROJECT_NAME, project_location=PROJECT_LOCATION) as flat_api:
 
-    
+    # from ghidra.program.model.listing import Program
+
     # set correct typing to leverage autocomplete  my_var: "ghidra-type"
-    prog: "Program" = flat_api.getCurrentProgram()    
+    prog: "Program" = flat_api.getCurrentProgram()
 
     print("Program Info:")
     program_name = prog.getName()
@@ -29,6 +29,10 @@ with pyghidra.open_program("/bin/ls", project_name=PROJECT_NAME, project_locatio
     compiler_spec_id = prog.getCompilerSpec().getCompilerSpecID()
     print("Program: {}: {}_{} ({})\n".format(program_name,
           language_id, compiler_spec_id, creation_date))
+
+    # Python 3 formatted string literals (f-strings)!!!
+    print(
+        f"Program: {program_name}: {language_id}_{compiler_spec_id} ({creation_date})")
 
     # Get info about the current program's memory layout
     print("Memory layout:")
